ca635203568299fbe13afd5f8902e073fc9acbc9
mizanur-rahaman-bd <sumon1581@gmail.com>
2025-01-10 19:23:36 +0600

    jani Na

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './NavBar.css'
+import { FaRegUser } from "react-icons/fa6";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { CiMenuBurger } from "react-icons/ci";
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+    <nav className='nav'>
+      <div className="container">
+        <div className="navRow">
+          <div className="navLogo">
+            <img src="Images/Logo.png" alt="logo" />
+
+          </div>
+          <div className="navContent">
+            <div className="navUser">
+            <FaRegUser />
+            <p>Log in</p>
+
+            </div>
+            <div className="navCart">
+            <MdOutlineShoppingCart />
+            <p>0</p>
+            </div>
+            <div className="navMenuBar">
+            <CiMenuBurger />
+            </div>
+            
+
+
+          </div>
+        </div>
+
+      </div>
+    </nav>
+    
+    </>
   )
 }
 
